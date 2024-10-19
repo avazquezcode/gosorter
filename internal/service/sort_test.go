a47190fe27319965ca84bf04ca5009d93e868f23
avazquezcode <avazquezcode@gmail.com>
2024-10-19 14:39:58 +0200

    Fix test

--- internal/service/sort_test.go
+++ internal/service/sort_test.go
@@ -13,43 +13,40 @@ func TestProcess(t *testing.T) {
 	tests := map[string]struct {
 		removeDuplicates bool
 		topLimit         int
-		expectsAscOrder  bool
+		expectsDescOrder bool
 		input            []string
 		expected         []string
 		expectsErr       bool
 	}{
 		"base case (default values)": {
-			input:           []string{"b", "c", "a", "a"},
-			expected:        []string{"a", "a", "b", "c"},
-			expectsAscOrder: true,
-			expectsErr:      false,
+			input:      []string{"b", "c", "a", "a"},
+			expected:   []string{"a", "a", "b", "c"},
+			expectsErr: false,
 		},
 		"removing duplicates (default values)": {
 			removeDuplicates: true,
 			input:            []string{"b", "c", "a", "a"},
 			expected:         []string{"a", "b", "c"},
-			expectsAscOrder:  true,
 			expectsErr:       false,
 		},
 		"limiting output": {
-			topLimit:        2,
-			input:           []string{"b", "c", "a"},
-			expected:        []string{"a", "b"},
-			expectsAscOrder: true,
-			expectsErr:      false,
+			topLimit:   2,
+			input:      []string{"b", "c", "a"},
+			expected:   []string{"a", "b"},
+			expectsErr: false,
 		},
 		"desc order is expected": {
-			input:           []string{"b", "c", "a"},
-			expected:        []string{"c", "b", "a"},
-			expectsAscOrder: false,
-			expectsErr:      false,
+			input:            []string{"b", "c", "a"},
+			expected:         []string{"c", "b", "a"},
+			expectsDescOrder: true,
+			expectsErr:       false,
 		},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			sorter, _ := sorting.Factory("default")
-			sortSvc := service.NewSortService(test.removeDuplicates, test.topLimit, sorter, test.expectsAscOrder)
+			sortSvc := service.NewSortService(test.removeDuplicates, test.topLimit, sorter, test.expectsDescOrder)
 			output, err := sortSvc.Process(test.input)
 			assert.Equal(t, test.expected, output)
 			assert.Equal(t, test.expectsErr, err != nil)
