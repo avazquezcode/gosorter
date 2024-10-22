0842aee68b060ebb1c47d79cd2053954bd96c8d6
avazquezcode <avazquezcode@gmail.com>
2024-10-22 20:59:40 +0200

    Add test for file package

---
+++ internal/file/file_test.go
@@ -0,0 +1,33 @@
+package file_test
+
+import (
+	"testing"
+
+	"github.com/avazquezcode/gosorter/internal/file"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLinesFromFile(t *testing.T) {
+	tests := map[string]struct {
+		fileName   string
+		expected   []string
+		expectsErr bool
+	}{
+		"valid file": {
+			fileName: "testdata/testfile.txt",
+			expected: []string{"a", "b", "c", "d", ""},
+		},
+		"file does not exist": {
+			fileName:   "testdata/testfile_404.txt",
+			expectsErr: true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			output, err := file.LinesFromFile(test.fileName)
+			assert.Equal(t, test.expected, output)
+			assert.Equal(t, test.expectsErr, err != nil)
+		})
+	}
+}
