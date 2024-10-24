7f3df3b80720bae415cf7df2d31aa3d623f922b3
avazquezcode <avazquezcode@gmail.com>
2024-10-24 23:53:56 +0200

    Remove unnecessary methods

--- internal/utils/slicetools/tools.go
+++ internal/utils/slicetools/tools.go
@@ -1,44 +1,5 @@
 package slicetools
 
-// RemoveDuplicates removes duplicates from a slice (it doesn't cause a side effect in the input)
-func RemoveDuplicates[T comparable](list []T) []T {
-	result := []T{}
-	set := make(map[T]struct{}, 0)
-	for _, item := range list {
-		if _, exist := set[item]; exist {
-			continue
-		}
-		set[item] = struct{}{}
-		result = append(result, item)
-	}
-
-	return result
-}
-
-// RemoveIfMatches removes items from a slice if matches to any item in the removableItems list (it doesn't cause a side effect in the input)
-func RemoveIfMatches[T comparable](list []T, removableItems []T) []T {
-	result := []T{}
-	set := make(map[T]struct{}, 0)
-	for _, item := range list {
-		shouldRemove := false
-		for _, removable := range removableItems {
-			if item == removable {
-				shouldRemove = true
-				break
-			}
-		}
-
-		if shouldRemove {
-			continue
-		}
-
-		set[item] = struct{}{}
-		result = append(result, item)
-	}
-
-	return result
-}
-
 // Reverse returns the reversed list (it doesn't cause a side effect in the input)
 func Reverse[T any](list []T) []T {
 	reversed := make([]T, len(list))
